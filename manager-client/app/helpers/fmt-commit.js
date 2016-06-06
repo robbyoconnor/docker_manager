74ad9d972fac1846963cbebc852e7b7c65cd5ff5
Robert O'Connor <rob@oconnor.ninja>
2016-06-05 21:28:10 -0400

    Link to the actual commit

--- manager-client/app/helpers/fmt-commit.js
+++ manager-client/app/helpers/fmt-commit.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Handlebars.makeBoundHelper(function(sha1, url) {
   if (Ember.isNone(url)) { return; }
-  return new Ember.Handlebars.SafeString("(<a href='" + url + "'>" + sha1 + "</a>)");
+  return new Ember.Handlebars.SafeString("(<a href='" + url +"/commit"+ sha1 "'>" + sha1 + "</a>)");
 });
